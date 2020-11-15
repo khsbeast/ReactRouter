a635285910ab16ef4299ad6d880f25cb87b9ea05
Kumar <harsh4981@gmail.com>
2020-11-15 17:36:44 +0530

    package added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-
 import Blog from './containers/Blog/Blog';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Blog />
       </div>
+      </BrowserRouter>
     );
   }
 }
